e3004e8bd643c87a9e049853de7f86f3888452cf
Stanislav Bronevytskyy <ssbron2@gmail.com>
2017-05-18 23:46:47 +0000

    Add changes

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$ (function() {
+	$('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+           if (target.length) {
+             $('html,body').stop().animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+    }
+});
+});
+
+$(function (){
+	new WOW().init();
+});
